31fd443a05e74e94f5adbeab348049ce58b88443
thechimaobii <thechimaobii@gmail.com>
2022-06-24 22:22:19 +0100

    first version

---
+++ index.js
@@ -0,0 +1,8 @@
+const myName = "Chima Ifeji";
+const myHeight= "6ft tall"
+const myCountry = "Nigeria"
+
+const sentence = "My name is " + myName + ". " + "I am a " + myHeight + " man from " + myCountry;
+
+
+alert(sentence);
